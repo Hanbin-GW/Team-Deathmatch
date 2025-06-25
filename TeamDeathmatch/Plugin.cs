daaa91fbd8eb6be7c204cdc8fb3d9749ccdc3f61
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-06-25 11:31:37 +0900

    release: 2.1.0

--- TeamDeathmatch/Plugin.cs
+++ TeamDeathmatch/Plugin.cs
@@ -27,7 +27,7 @@ namespace TeamDeathmatch
     {
         public override string Name => "Team deathmatch";
         public override string Author => "Hanbin-GW";
-        public override Version Version { get; } = new Version(2, 0, 1);
+        public override Version Version { get; } = new Version(2, 1, 0);
         
         public Dictionary<Player, string> playerTeams = new();
         public List<Player> WaitingPlayers = new();
