1194dfd7c4a59fc2fddb4d45677e328a7c65d83e
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-08-06 20:43:23 +0900

    audio path

--- TeamDeathmatch/EventHandlers/AnnouncerEventHandlers.cs
+++ TeamDeathmatch/EventHandlers/AnnouncerEventHandlers.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Linq;
 using Exiled.API.Features;
 using PlayerRoles;
@@ -7,17 +9,51 @@ namespace TeamDeathmatch.EventHandlers
     public class AnnouncerEventHandlers
     {
         public Plugin Plugin;
+        public readonly string AudioDirectory;
+
+        public AnnouncerEventHandlers()
+        {
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            AudioDirectory = Path.Combine(appDataPath, "EXILED", "Plugins", "DeathMatch");
+        }
         
         private void RunReversalEvent(string newLeadingTeam)
         {
             foreach (var player in Player.List)
             {
-                player.ShowHint($"<color=yellow>{newLeadingTeam} 어나운서: 우리가 선두를 잡았다!</color>");
+                AudioPlayer audioPlayerWinning = AudioPlayer.CreateOrGet(
+                    $"Spectator AudioPlayer",
+                    condition: (hub) =>
+                    {
+                        Player player = new Player(hub);
+                        return player != null
+                               && Plugin.Instance.playerTeams.ContainsKey(player)
+                               && Plugin.Instance.playerTeams[player] == newLeadingTeam;
+                    },
+                    onIntialCreation: (p) =>
+                    {
+                        Speaker speaker = p.AddSpeaker("Main", isSpatial: true, maxDistance: 5000f);
+                    });
+                
+                AudioPlayer audioPlayerLosing = AudioPlayer.CreateOrGet(
+                    $"Spectator AudioPlayer",
+                    condition: (hub) =>
+                    {
+                        Player player = new Player(hub);
+                        return player != null
+                               && Plugin.Instance.playerTeams.ContainsKey(player)
+                               && Plugin.Instance.playerTeams[player] != newLeadingTeam;
+                    },
+                    onIntialCreation: (p) =>
+                    {
+                        Speaker speaker = p.AddSpeaker("Main", isSpatial: true, maxDistance: 5000f);
+                    });
             }
-
+            
             foreach (var player in Player.List.Where(p => GetTeamName(p) == newLeadingTeam))
             {
                 //player.AddItem(ItemType.GunE11SR);
+                //player.ShowHint($"<color=yellow>{newLeadingTeam} 어나운서: 우리가 선두를 잡았다!</color>");
             }
 
         }
