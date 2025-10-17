db4a0414e59a127c737d49d367cd555e1a7fcb33
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-10-17 11:35:22 +0900

    change a main

--- TeamDeathmatch/Plugin.cs
+++ TeamDeathmatch/Plugin.cs
@@ -32,7 +32,7 @@ namespace TeamDeathmatch
     {
         public override string Name => "Team deathmatch";
         public override string Author => "Hanbin-GW";
-        public override Version Version { get; } = new Version(3, 0, 1);
+        public override Version Version { get; } = new Version(3, 0, 2);
         public Dictionary<Player, string> playerTeams = new();
         public List<Player> WaitingPlayers = new();
         public bool TdmStarted = false;
